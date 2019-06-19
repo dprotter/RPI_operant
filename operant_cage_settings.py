38adf0bafead64ad6cf03dd23782447bde944916
dprotter <david.protter@gmail.com>
2019-06-19 13:09:03 -0600

    yah

--- operant_cage_settings.py
+++ operant_cage_settings.py
@@ -1,3 +1,5 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
 pins = {'lever_food':4,'lever_social':17,'led_food':18, 'read_pellet':24,
     'pellet_tone':21, 'start_tone':20}
 
