ed8eaab9ff72617433be08022677acf414529f30
dprotter <david.protter@gmail.com>
2019-06-19 16:46:30 -0600

    door shaping

--- operant_cage_settings.py
+++ operant_cage_settings.py
@@ -1,7 +1,7 @@
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 pins = {'lever_food':4,'lever_social':17,'led_food':18, 'read_pellet':24,
-    'pellet_tone':21, 'start_tone':20}
+    'pellet_tone':21, 'start_tone':20, 'round_end_buzzer':22, 'led_social':18,}
 
 
 #values Levers [extended, retracted]
@@ -10,7 +10,8 @@ lever_angles = {'food':[40, 115], 'social':[40,100]}
 
 continuous_servo_speeds = {
                         'dispense_pellet':{'stop':0.04, 'fwd':0.09},
-                        'door':{'stop':0.04, 'open':0.9, 'close':0-0.9+0.04}
+                        'door':{'stop':0.04, 'open':0.9, 'close':0-0.9+0.04
+                                'open time':1.25, 'close time':2.7}
                                                                             }
 
 
