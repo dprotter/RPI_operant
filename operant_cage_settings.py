72b4cb173c709d6cb82f6fb107a25f2ca13f5de2
dprotter <david.protter@gmail.com>
2019-06-19 16:48:58 -0600

    yah

--- operant_cage_settings.py
+++ operant_cage_settings.py
@@ -1,7 +1,7 @@
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 pins = {'lever_food':4,'lever_social':17,'led_food':18, 'read_pellet':24,
-    'pellet_tone':21, 'start_tone':20, 'round_end_buzzer':22, 'led_social':18,}
+    'pellet_tone':21, 'start_tone':20, 'door_close_buzzer':22, 'led_social':19,}
 
 
 #values Levers [extended, retracted]
