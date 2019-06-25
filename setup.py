3d4ce92673c554be85a21306b346befe1ddaad1a
dprotter <david.protter@gmail.com>
2019-06-25 13:09:02 -0600

    w

--- setup.py
+++ setup.py
@@ -22,12 +22,11 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 
-thelibFolder = os.path.dirname(os.path.realpath(__file__))
-requirementPath = thelibFolder + '/requirements.txt'
-REQUIRED = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
-if os.path.isfile(requirementPath):
+
+
+if os.path.isfile('requirements.txt'):
     with open(requirementPath) as f:
-        REQUIRED = f.read().splitlines()
+        REQUIRED = [line for f.readline() in f]
 print(REQUIRED)
 
 # Where the magic happens:
