b1adb81d888c9b935e6c4997939ddd956cb41b7b
dprotter <david.protter@gmail.com>
2019-06-25 12:36:58 -0600

    fixing setup script

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ REQUIRED = [
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
