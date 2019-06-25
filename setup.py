fb567dee5f7615b492f3b0721f67a83f4116e2c4
dprotter <david.protter@gmail.com>
2019-06-25 12:48:37 -0600

    fixing install script with requirements file

--- setup.py
+++ setup.py
@@ -21,10 +21,13 @@ REQUIRES_PYTHON = '>=3.4.0'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    'numpy', 'RPi.GPIO', 'adafruit-circuitpython-servokit', 'netifaces', 'smtplib',
-]
 
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+REQUIRED = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        REQUIRED = f.read().splitlines()
 
 
 # Where the magic happens:
