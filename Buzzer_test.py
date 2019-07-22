6ce72578e323455f4946c1242a7a301c523dcf0f
dprotter <david.protter@gmail.com>
2019-07-22 17:45:57 -0600

    more fix more better

--- Buzzer_test.py
+++ Buzzer_test.py
@@ -3,33 +3,27 @@ from operant_cage_settings import pins, servo_dict, continuous_servo_speeds, lev
 import time
 import os
 import RPi.GPIO as GPIO
-os.system('sudo pigpiod')
-GPIO.setmode(GPIO.BCM)
+#activates the pigpio daemon that runs PWM, unless its already running
+if os.system('sudo lsof -i TCP:8888'):
+    os.system('sudo pigpio')
+
 pi = pigpio.pi()
 
 
-GPIO.setup(6, GPIO.OUT)
-
-
-print('software pwm')
 print('2k')
-pi.set_PWM_dutycycle(12, 255/2)
-pi.set_PWM_frequency(12, 2000)
+pi.set_PWM_dutycycle(pins['pellet_tone'], 255/2)
+pi.set_PWM_frequency(pins['pellet_tone'], 2000)
 
-time.sleep(2)
-pi.hardware_PWM(12, 3000, 0)
-time.sleep(1)
+
 
 print('4k')
 
-pi.set_PWM_dutycycle(12, 255/2)
-pi.set_PWM_frequency(12, 4000)
+pi.set_PWM_dutycycle(pins['pellet_tone'], 255/2)
+pi.set_PWM_frequency(pins['pellet_tone'], 4000)
 
-print("LED")
-GPIO.output(6, 1)
+
 time.sleep(2)
-pi.hardware_PWM(12, 4000, 0)
+pi.hardware_PWM(pins['pellet_tone'], 4000, 0)
 
-GPIO.output(6,0)
+
 print('all done')
-GPIO.cleanup()
