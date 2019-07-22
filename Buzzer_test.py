31feb2c8987bd3db2d80e5adeb0cd89dd05c78b6
dprotter <david.protter@gmail.com>
2019-07-22 16:14:44 -0600

    further testing

--- Buzzer_test.py
+++ Buzzer_test.py
@@ -2,13 +2,13 @@ import pigpio
 from operant_cage_settings import pins, servo_dict, continuous_servo_speeds, lever_angles
 import time
 import os
-
+import RPi.GPIO as GPIO
 os.system('sudo pigpiod')
-
+GPIO.set_mode(GPIO.BCM)
 pi = pigpio.pi()
 
 
-
+GPIO.setup(6, GPIO.OUT)
 
 
 print('software pwm')
@@ -25,7 +25,11 @@ print('4k')
 pi.set_PWM_dutycycle(12, 255/2)
 pi.set_PWM_frequency(12, 4000)
 
-time.sleep(1)
+print("LED")
+GPIO.output(6, 1)
+time.sleep(2)
 pi.hardware_PWM(12, 4000, 0)
+
+GPIO.output(6,0)
 print('all done')
 GPIO.cleanup()
