bb419747b64dea9aa59ff1f6eeef99bb8b38c453
dprotter <david.protter@gmail.com>
2019-08-15 16:59:33 -0600

    fix test door test

--- Test_Door_Test.py
+++ Test_Door_Test.py
@@ -527,12 +527,6 @@ for i in range(loops):
         time.sleep(1)
         do_stuff_queue.put(('close door',))
         print('time to move that vole over!')
-    #reset our global values interrupt and monitor. This will turn off the lever
-    #if it is still being monitored. This resets the inerrupt value for the next
-    #loop of the training.
-    interrupt = False
-    monitor = False
-
 
         timestamp_queue.put('%i, start of move animal time, %f'%(round, time.time()-start_time))
         for i in range(move_animal_time):
@@ -542,6 +536,14 @@ for i in range(loops):
         print('vole should be moved now')
 
         time.sleep(time_after_move)
+    #reset our global values interrupt and monitor. This will turn off the lever
+    #if it is still being monitored. This resets the inerrupt value for the next
+    #loop of the training.
+    interrupt = False
+    monitor = False
+
+
+
 
 
 
