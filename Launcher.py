6daaf5746d43886d0478142c2e8d27e6c80cef8e
dprotter <david.protter@gmail.com>
2020-02-25 15:24:39 -0700

    null vals

--- Launcher.py
+++ Launcher.py
@@ -183,7 +183,7 @@ spec.loader.exec_module(module)
 while user_accepts == False:
 
     #if the user has defined modified variables, update the module.
-    if experiment_status.iloc[next_exp].modified_vars!=None:
+    if not experiment_status.iloc[next_exp].modified_var.isna:
         print(f'{experiment_status.iloc[next_exp].modified_vars}')
         update_vars(experiment_status.iloc[next_exp].modified_vars)
 
