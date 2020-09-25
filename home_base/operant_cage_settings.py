66e8931728ab5cf21b15159e2539823b3549d3aa
dprotter <david.protter@gmail.com>
2020-09-25 12:03:07 -0600

    setting up a local RPI_operant settings and a default settings file

--- home_base/operant_cage_settings.py
+++ home_base/operant_cage_settings.py
@@ -1,37 +1,22 @@
 from adafruit_servokit import ServoKit
+import os
+
 kit = ServoKit(channels=16)
-pins = {'lever_food':24,
-    'lever_door_1':27,
-    'lever_door_2':22,
-    'led_food':0,
-    'read_pellet':13,
-    'speaker_tone':21,
-    'led_social':19,
-    'door_1_override_open_switch':4,
-    'door_2_override_open_switch':16,
-    'door_1_override_close_switch':23,
-    'door_2_override_close_switch':25,
-    'door_1_state_switch':17,
-    'door_2_state_switch':18,
 
-    'gpio_sync':12, }
+if not os.path.isfile('/etc/RPI_operant/operant_cage_settings_local.py'):
+    print('no local operant cage settings, reverting to defaults')
 
+    from home_base.operant_cage_settings_defaults import pins, lever_angles, continuous_servo_speeds, servo_dict
 
-#values Levers [extended, retracted]
-lever_angles = {'food':[100, 50], 'door_1':[70,120], 'door_2':[85,35]}
+else:
+    print('importing local operant cage settings')
+    import importlib.util
+    spec = importlib.util.spec_from_file_location("operant_cage_settings_local", 
+                                                    "/etc/RPI_operant/operant_cage_settings_local.py")
+    foo = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(foo)
 
-
-continuous_servo_speeds = {
-                        'dispense_pellet':{'stop':0.15, 'fwd':0.12},
-                        'door_1':{'stop':0.16, 'close':0.8, 'open':-0.1,
-                        'open time':1.6,
-                        },
-                        'door_2':{'stop':0.1, 'open':0.8, 'close':-0.1,
-                        'open time':1.6,
-                        }
-                                                                            }
-
-
-servo_dict = {'lever_food':kit.servo[14], 'dispense_pellet':kit.continuous_servo[1],
-                'lever_door_1':kit.servo[2], 'door_1':kit.continuous_servo[0],
-                'lever_door_2':kit.servo[12],'door_2':kit. continuous_servo[13]}
+    pins = foo.pins
+    lever_angles = foo.lever_angles
+    continuous_servo_speeds = foo.continuous_servo_speeds
+    servo_dict = foo.servo_dict
---
+++ home_base/operant_cage_settings_defaults.py
@@ -0,0 +1,43 @@
+from adafruit_servokit import ServoKit
+import os
+
+kit = ServoKit(channels=16)
+
+'''dont change these default values! if you need to make changes to the operant cage settings,
+like changing lever angle values or servo speeds, copy '''
+
+pins = {'lever_food':24,
+    'lever_door_1':27,
+    'lever_door_2':22,
+    'led_food':0,
+    'read_pellet':13,
+    'speaker_tone':21,
+    'led_social':19,
+    'door_1_override_open_switch':4,
+    'door_2_override_open_switch':16,
+    'door_1_override_close_switch':24,
+    'door_2_override_close_switch':25,
+    'door_1_state_switch':17,
+    'door_2_state_switch':18,
+
+    'gpio_sync':6, }
+
+
+#values Levers [extended, retracted]
+lever_angles = {'food':[100, 50], 'door_1':[70,120], 'door_2':[85,35]}
+
+
+continuous_servo_speeds = {
+                        'dispense_pellet':{'stop':0.15, 'fwd':0.12},
+                        'door_1':{'stop':0.16, 'close':0.8, 'open':-0.1,
+                        'open time':1.6,
+                        },
+                        'door_2':{'stop':0.1, 'open':0.8, 'close':-0.1,
+                        'open time':1.6,
+                        }
+                                                                            }
+
+
+servo_dict = {'lever_food':kit.servo[14], 'dispense_pellet':kit.continuous_servo[1],
+                'lever_door_1':kit.servo[2], 'door_1':kit.continuous_servo[0],
+                'lever_door_2':kit.servo[12],'door_2':kit. continuous_servo[13]}
