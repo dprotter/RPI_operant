0abe2935388d7f178f4efcd7112c9cf062171a5a
dprotter <david.protter@gmail.com>
2020-10-05 12:26:55 -0600

    fixing disparate tones across scripts

--- run_scripts/Door_shape.py
+++ run_scripts/Door_shape.py
@@ -22,15 +22,15 @@ key_values = {'num_rounds': 0,
               'time_II':30,
               'move_time':20,
               'pellet_tone_time':1, 
-              'pellet_tone_hz':5000,
+              'pellet_tone_hz':2500,
               'door_close_tone_time':1, 
               'door_close_tone_hz':7000,
               'door_open_tone_time':1,
               'door_open_tone_hz':10000,
               'round_start_tone_time':1, 
-              'round_start_tone_hz':3000,
+              'round_start_tone_hz':5000,
               'delay by day':[0,0,1,1,1],
-              'delay default':2}
+              'delay default':1}
 
 key_values['num_rounds'] = key_values['repetitions'] * key_values['sets']
 
