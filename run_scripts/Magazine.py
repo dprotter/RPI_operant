240fc922430be6b6b7ba8386bee956d48511489a
Gabe Chapel <gach1192@colorado.edu>
2021-03-29 15:57:44 -0600

    running first actual training

--- run_scripts/Magazine.py
+++ run_scripts/Magazine.py
@@ -70,7 +70,7 @@ def run_script():
                     'hz':key_values['round_start_tone_hz'],
                     'name':'round_start_tone'}
 
-    pellet_buzz = {'buzz_length':key_values['pellet_tone_time'],
+    pellet_buzz = {'buzz_length':key_values/['pellet_tone_time'],
                     'hz':key_values['pellet_tone_hz'],
                     'name':'pellet_tone'}
 
