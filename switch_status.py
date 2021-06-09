c27aacc18208077b160857e7098d24f008f52290
David Protter <david.protter@gmail.com>
2021-06-09 10:29:48 -0600

    commit to push to other boxes

--- switch_status.py
+++ switch_status.py
@@ -1,4 +1,6 @@
-import home_base.functions as FN
+import sys
+sys.path.append('/home/pi/')
+import RPI_operant.home_base.functions as FN
 fn = FN.runtime_functions()
 from home_base.functions import pins, GPIO
 from tabulate import tabulate
