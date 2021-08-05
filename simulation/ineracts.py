b23a27eedf6c337bb7f000c8d83d300a1804552d
ryca1116 <ryca1116@colorado.edu>
2021-08-05 15:59:32 -0600

    Started simulations

---
+++ simulation/ineracts.py
@@ -0,0 +1,22 @@
+""" 
+Date Created : 8/5/2021
+Date Modified: 8/5/2021
+Author: Ryan Cameron, David Protter, Sarah ???
+Description: This is the script that has all of the interactable objects for the vole
+Property of Donaldson Lab, University of Colorado Boulder, PI Zoe Donaldson
+http://www.zdonaldsonlab.com/
+Code located at - https://github.com/donaldsonlab/Operant-Cage 
+"""
+
+# Imports
+
+# Levers
+class lever:
+    def __init__(self, lever_ID, )
+# Food Dispenser
+
+# Doors
+
+# IR's
+
+# Vole (Test and Tethered)
---
+++ simulation/main.py
@@ -0,0 +1,24 @@
+""" 
+Date Created : 8/5/2021
+Date Modified: 8/5/2021
+Author: Ryan Cameron, David Protter, Sarah ???
+Description: This script is the main wrapper for the simulation of the operant cage hardware. It connects to the actual operant cage hardware, and simulates a vole going through the different tests, so that the pipeline from the raspberry pi to the arduino and into bonsai can be tested.
+
+Property of Donaldson Lab, University of Colorado Boulder, PI Zoe Donaldson
+http://www.zdonaldsonlab.com/
+Code located at - https://github.com/donaldsonlab/Operant-Cage 
+"""
+
+# Imports
+
+# Create vole object
+
+# Import the vole settings for how it behaves
+
+# Set starting vole position
+
+# Create pipeline to read all the switch statuses
+
+# Depending on the status of switches have the vole make a decision
+
+# Repeat for the whole duration of the simulation
