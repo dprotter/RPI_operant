f280be23de2a410afcd2d416ad566ac960d5fbb0
ryca1116 <ryca1116@colorado.edu>
2021-08-26 15:40:18 -0600

    Setup

--- README.md
+++ README.md
@@ -46,3 +46,41 @@ When done, you should see this confirmation screen and can exit out of the confi
 With these options selected, restart the pi to confirm these changes with:
 
     sudo reboot
+
+As a final confirmation make sure that the serial0 port exists by using the follwing command:
+
+    ls /dev
+
+The /dev/serial0 port is the one that is connected to the Tx and Rx pins on the raspberry pi, which are the pins that will be connected to a software serial port on the Arduino.
+
+## Arduino Setup
+
+Once the pi is setup for serial communication, you must setup the Arduino to recieve the serial commands from the pi and then send the correct signal to the bonsai system.
+
+The first step in this proccess is to connect the Arduino the PC with bonsai loaded onto it and load in the .ino script located at *commHandler-2.0/commHandler-2.0.ino*.
+
+By default, this script sets up a software serial port to read info in using pins **2** and **3**. NOTE: these pins can always be changed in the script by changing the following two lines of code:
+
+    int softRX = 2;
+    int softTX = 3;
+
+Since pin **2** is set up as the recieving pin, connect the **Tx** pin on the Pi to pin **2** on the Arduino. Since the Arduino will not be sending anything back to the Raspberry Pi, **DO NOT CONNECT PIN 3 TO RX ON THE PI**, doing so could short out the Pi as the Arduino send in 5V and the Pi recieves in 3.3V.
+
+Finally, make sure that you have the SoftwareSerial Arduino library loaded on the Arduino from your PC. If you do not, simply install it using the library manager in the Arduino IDE app.
+
+### Testing
+To test the Arduino's command handling capabilities, look through the follwing lines of code in the *commandHandler-2.0.ino* lines of code to find which pins are set to output for the commands you want to test.
+
+    byte pins[] = {13,10,9,4,5,6,7,11};
+
+    int leverOutFoodPin      = pins[0];
+    int leverOutPartnerPin   = pins[1];
+    int leverOutNovelPin     = pins[2];
+    int leverPressFoodPin    = pins[3];
+    int leverPressPartnerPin = pins[4];
+    int leverPressNovelPin   = pins[5];
+    int enterPartnerPin      = pins[6];
+    int enterNovelPin        = pins[7];
+    int nullPin              = pins[8];
+
+With the correct pin numbers located, connect that pin on the Arduino to a breadoard with a standard LED and a 220 Ohm resistor. 
---
+++ commHandler-2.0/commHandler-2.0.ino
@@ -0,0 +1,107 @@
+// commHandler is a script that takes the serial command from the raspberry pi and sends the correct pulse along the correct pin to the bonsai software.
+
+// includes
+//#include <CSV_Parser.h>
+#include <SoftwareSerial.h>
+
+byte pins[] = {13,10,9,4,5,6,7,11};
+
+int leverOutFoodPin      = pins[0];
+int leverOutPartnerPin   = pins[1];
+int leverOutNovelPin     = pins[2];
+int leverPressFoodPin    = pins[3];
+int leverPressPartnerPin = pins[4];
+int leverPressNovelPin   = pins[5];
+int enterPartnerPin      = pins[6];
+int enterNovelPin        = pins[7];
+int nullPin              = pins[8];
+
+String sdata = ""; // Initialize
+byte pinCount = sizeof(pins);
+
+// Software serial pins (rx, Tx)
+int softRX = 2;
+int softTX = 3;
+SoftwareSerial myserial(softRX,softTX);
+
+void setup () {
+    // Set the right pins for the right commands
+    for (byte i = 0; i < pinCount; i++) {
+        pinMode(pins[i], OUTPUT);
+    }
+
+    // setup the serial
+    Serial.begin(9600);
+    Serial.println("Starting Serial Dialogue");
+
+    // Setup the software serial port
+    myserial.begin(9600);
+    
+    
+    // Test the csv parsing
+    //CSV_Parser cp('commands.csv', /*format*/, "ss", /*has_header*/, false);
+    //cp.print();
+}
+
+void loop () {
+    // Read the serial for the command
+    byte ch;
+    if (myserial.available()) {
+        ch = myserial.read();
+        sdata += (char)ch;
+
+        if (ch == '\r') { // End of the command, full line has been recieved and is ready to go
+            sdata.trim();
+
+            // Send to the command handler
+            commands(sdata);
+
+            // Re-initialize the variable
+            sdata = "";
+        }
+    }
+}
+
+void commands (String command) {
+    if (command == "leverOutFood") {
+      // Food Lever is extended
+      digitalWrite(leverOutFoodPin, HIGH);
+    }
+    else if (command == "leverOutPartner") {
+      // Partner lever is extended
+      digitalWrite(leverOutPartnerPin, HIGH);
+    }
+    else if (command == "leverOutNovel") {
+      // Novel lever is extended
+      digitalWrite(leverOutNovelPin, HIGH);
+    }
+    else if (command == "leverPressFood") {
+      // Food lever is pressed
+      digitalWrite(leverPressFoodPin, HIGH);
+    }
+    else if (command == "leverPressPartner") {
+      // Partner lever is pressed
+      digitalWrite(leverPressPartnerPin, HIGH);
+    }
+    else if (command == "leverPressNovel") {
+      // Novel lever is pressed
+      digitalWrite(leverPressNovelPin, HIGH);
+    }
+    else if (command == "enterNovel") {
+      // Animal entered the novel chamber
+      digitalWrite(enterNovelPin, HIGH);
+    }
+    else if (command == "enterPartner") {
+      // Animal entered the partner chamber
+      digitalWrite(enterPartnerPin, HIGH);
+    }
+    else {
+      digitalWrite(nullPin, HIGH);
+    }
+
+    // Reset all the values to LOW
+    delay(500);
+    for (byte i = 0; i < pinCount; i++) {
+        digitalWrite(pins[i], LOW);
+    }
+}
