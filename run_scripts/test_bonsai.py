4bf747fa32c812de78caa2cbae48addd379a19ed
ryca1116 <ryca1116@colorado.edu>
2021-08-31 16:28:30 -0600

    updated commands

--- run_scripts/test_bonsai.py
+++ run_scripts/test_bonsai.py
@@ -7,12 +7,12 @@ coms = sender()
 coms.start()
 
 if __name__ == '__main__':
-    coms.send_data('leverOutFood')
+    coms.send_data('lever_out_food')
     
     while coms.busy():
         print('holding for coms')
         time.sleep(0.25)
-    coms.send_data('leverPressNovel')
+    coms.send_data('lever_press_door_1')
     
     while coms.busy():
         print('holding for coms')
