ffc0b08e9ef74d4234243173137cddf0860842b5
Ryan Cameron <ryca1116@colorado.edu>
2021-09-21 09:50:15 -0600

    sent the correct arduino script

--- commHandler-2.0/commHandler-2.0.ino
+++ commHandler-2.0/commHandler-2.0.ino
@@ -1,20 +1,23 @@
 // commHandler is a script that takes the serial command from the raspberry pi and sends the correct pulse along the correct pin to the bonsai software.
 
 // includes
-//#include <CSV_Parser.h>
 #include <SoftwareSerial.h>
+#include <Firmata.h>
 
-byte pins[] = {13,10,9,4,5,6,7,11};
+byte pins[] = {13,10,9,4,5,6,7,11,8};
+int vals[] = {25,50,100,150,200,250,300,350,400};
 
-int leverOutFoodPin      = pins[0];
-int leverOutPartnerPin   = pins[1];
-int leverOutNovelPin     = pins[2];
-int leverPressFoodPin    = pins[3];
-int leverPressPartnerPin = pins[4];
-int leverPressNovelPin   = pins[5];
-int enterPartnerPin      = pins[6];
-int enterNovelPin        = pins[7];
-int nullPin              = pins[8];
+int lever_out_food_pin      = pins[0];
+int lever_out_door_1_pin    = pins[1];
+int lever_out_door_2_pin    = pins[2];
+int lever_press_food_pin    = pins[3];
+int lever_press_door_1_pin  = pins[4];
+int lever_press_door_2_pin  = pins[5];
+int cross_door_1_pin        = pins[6];
+int cross_door_2_pin        = pins[7];
+int pellet_retrieved_pin    = pins[8];
+int messagePin              = A1;
+
 
 String sdata = ""; // Initialize
 byte pinCount = sizeof(pins);
@@ -31,16 +34,29 @@ void setup () {
     }
 
     // setup the serial
-    Serial.begin(9600);
+    Serial.begin(57600);
     Serial.println("Starting Serial Dialogue");
 
     // Setup the software serial port
     myserial.begin(9600);
-    
+
+    for(int i = 0; i<4; i++){
+      digitalWrite(lever_out_food_pin, HIGH);
+      digitalWrite(lever_press_door_1_pin, HIGH);
+      delay(500);
+      digitalWrite(lever_out_food_pin, LOW);
+      digitalWrite(lever_press_door_1_pin, LOW);
+      delay(500);
+    }
     
     // Test the csv parsing
     //CSV_Parser cp('commands.csv', /*format*/, "ss", /*has_header*/, false);
     //cp.print();
+
+    // Setup the Firmata
+    Firmata.setFirmwareVersion(FIRMATA_MAJOR_VERSION, FIRMATA_MINOR_VERSION);
+    Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
+    Firmata.begin(57600);
 }
 
 void loop () {
@@ -49,7 +65,7 @@ void loop () {
     if (myserial.available()) {
         ch = myserial.read();
         sdata += (char)ch;
-
+            
         if (ch == '\r') { // End of the command, full line has been recieved and is ready to go
             sdata.trim();
 
@@ -59,49 +75,83 @@ void loop () {
             // Re-initialize the variable
             sdata = "";
         }
+        else {
+          // Send a 0 value to bonsai
+          //Firmata.sendAnalog(messagePin, 0);
+        }
+    }
+    else {
+      // Send a 0 value to bonsai
+      //Firmata.sendAnalog(messagePin, 0);
     }
 }
 
 void commands (String command) {
-    if (command == "leverOutFood") {
+    if (command == "lever_out_food") {
       // Food Lever is extended
-      digitalWrite(leverOutFoodPin, HIGH);
+      Firmata.sendAnalog(messagePin  , vals[0]);
+      digitalWrite(lever_out_food_pin  , HIGH);
+      //delay(50);
     }
-    else if (command == "leverOutPartner") {
+    else if (command == "lever_out_door_1") {
       // Partner lever is extended
-      digitalWrite(leverOutPartnerPin, HIGH);
+      Firmata.sendAnalog(messagePin, vals[1]);
+      digitalWrite(lever_out_door_1_pin, HIGH);
+      //delay(50);
     }
-    else if (command == "leverOutNovel") {
+    else if (command == "lever_out_door_2") {
       // Novel lever is extended
-      digitalWrite(leverOutNovelPin, HIGH);
+      Firmata.sendAnalog(messagePin, vals[2]);
+      digitalWrite(lever_out_door_2_pin, HIGH);
+      //delay(50);
     }
-    else if (command == "leverPressFood") {
+    else if (command == "lever_press_food") {
       // Food lever is pressed
-      digitalWrite(leverPressFoodPin, HIGH);
+      Firmata.sendAnalog(messagePin, vals[3]);
+      digitalWrite(lever_press_food_pin, HIGH);
+      //delay(50);
     }
-    else if (command == "leverPressPartner") {
+    else if (command == "lever_press_door_1") {
       // Partner lever is pressed
-      digitalWrite(leverPressPartnerPin, HIGH);
+      Firmata.sendAnalog(messagePin, vals[4]);
+      digitalWrite(lever_press_door_1_pin, HIGH);
+      //delay(50);
     }
-    else if (command == "leverPressNovel") {
+    else if (command == "lever_press_door_2") {
       // Novel lever is pressed
-      digitalWrite(leverPressNovelPin, HIGH);
+      Firmata.sendAnalog(messagePin, vals[5]);
+      digitalWrite(lever_press_door_2_pin, HIGH);
+      //delay(50);
     }
-    else if (command == "enterNovel") {
+    else if (command == "cross_door_1") {
       // Animal entered the novel chamber
-      digitalWrite(enterNovelPin, HIGH);
+      Firmata.sendAnalog(messagePin, vals[6]);
+      digitalWrite(cross_door_1_pin, HIGH);
+      //delay(50);
     }
-    else if (command == "enterPartner") {
+    else if (command == "cross_door_2") {
       // Animal entered the partner chamber
-      digitalWrite(enterPartnerPin, HIGH);
+      Firmata.sendAnalog(messagePin, vals[7]);
+      digitalWrite(cross_door_2_pin, HIGH);
+      //delay(50);
+    }
+    else if (command == "pellet_retrieved") {
+      // Animal entered the partner chamber
+      Firmata.sendAnalog(messagePin, vals[8]);
+      digitalWrite(cross_door_2_pin, HIGH);
+      //delay(50);
     }
     else {
-      digitalWrite(nullPin, HIGH);
+      // Send a 0 value
     }
 
     // Reset all the values to LOW
-    delay(500);
+    //delay(2000);
     for (byte i = 0; i < pinCount; i++) {
         digitalWrite(pins[i], LOW);
     }
+}
+
+void analogWriteCallback(byte pin, int value) {
+  analogWrite(pin, value);
 }
